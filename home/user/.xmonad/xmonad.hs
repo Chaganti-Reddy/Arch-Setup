fc74eff1c668d41468644cbabcb7682039bcf6c7
Chaganti Reddy <chagantivenkataramireddy4@gmail.com>
2022-05-13 15:01:21 +0530

    Update

--- home/user/.xmonad/xmonad.hs
+++ home/user/.xmonad/xmonad.hs
@@ -132,6 +132,7 @@ myStartupHook = do
     spawnOnce "xinput set-prop 'DLL09D9:00 04F3:3147 Touchpad' 'libinput Tapping Enabled' 1"
     spawnOnce "xinput set-prop 'DLL09D9:00 04F3:3147 Touchpad' 'libinput Natural Scrolling Enabled' 1"
     -- spawnOnce "redshift-gtk"
+    spawnOnce "numlockx"
 
     -- spawn ("sleep 2 && conky -c $HOME/.config/conky/xmonad/" ++ colorScheme ++ "-01.conkyrc")
     spawn ("sleep 2 && trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 " ++ colorTrayer ++ " --height 22")
