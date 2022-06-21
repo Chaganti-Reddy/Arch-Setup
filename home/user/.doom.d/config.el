adc8aa59e0a9c2933b3fe416bfac89c7e0ca6a94
Chaganti Reddy <chagantivenkataramireddy4@gmail.com>
2022-06-21 14:57:53 +0530

    update

--- home/user/.doom.d/config.el
+++ home/user/.doom.d/config.el
@@ -2042,29 +2042,29 @@ is selected, only the bare key is returned."
 
 ;; Display errors and warnings in an org-mode in seperate buffer.
 
-(defvar org-babel-eval-verbose t
-  "A non-nil value makes `org-babel-eval' display")
+;; (defvar org-babel-eval-verbose t
+;;   "A non-nil value makes `org-babel-eval' display")
 
-(defun org-babel-eval (cmd body)
-  "Run CMD on BODY.
-  If CMD succeeds then return its results, otherwise display
-  STDERR with `org-babel-eval-error-notify'."
-  (let ((err-buff (get-buffer-create " *Org-Babel Error*")) exit-code)
-    (with-current-buffer err-buff (erase-buffer))
-    (with-temp-buffer
-      (insert body)
-      (setq exit-code
-            (org-babel--shell-command-on-region
-             (point-min) (point-max) cmd err-buff))
-      (if (or (not (numberp exit-code)) (> exit-code 0)
-              (and org-babel-eval-verbose (> (buffer-size err-buff) 0))) ; new condition
-          (progn
-            (with-current-buffer err-buff
-              (org-babel-eval-error-notify exit-code (buffer-string)))
-            nil)
-        (buffer-string)))))
+;; (defun org-babel-eval (cmd body)
+;;   "Run CMD on BODY.
+;;   If CMD succeeds then return its results, otherwise display
+;;   STDERR with `org-babel-eval-error-notify'."
+;;   (let ((err-buff (get-buffer-create " *Org-Babel Error*")) exit-code)
+;;     (with-current-buffer err-buff (erase-buffer))
+;;     (with-temp-buffer
+;;       (insert body)
+;;       (setq exit-code
+;;             (org-babel--shell-command-on-region
+;;              (point-min) (point-max) cmd err-buff))
+;;       (if (or (not (numberp exit-code)) (> exit-code 0)
+;;               (and org-babel-eval-verbose (> (buffer-size err-buff) 0))) ; new condition
+;;           (progn
+;;             (with-current-buffer err-buff
+;;               (org-babel-eval-error-notify exit-code (buffer-string)))
+;;             nil)
+;;         (buffer-string)))))
 
-(setq org-babel-eval-verbose t)
+;; (setq org-babel-eval-verbose t)
 
 (use-package lsp-treemacs
   :after lsp)
