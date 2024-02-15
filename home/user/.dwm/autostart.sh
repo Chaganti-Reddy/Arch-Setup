f67c148245b33b4589419b73696c09803a164d59
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2024-02-15 19:16:43 +0530

    Adding Dmenu Scripts to open File from Dmenu

--- home/user/.dwm/autostart.sh
+++ home/user/.dwm/autostart.sh
@@ -6,6 +6,7 @@
 numlockx &
 nm-applet &
 dunst &
+clipmenud &
 xinput set-prop "DLL09D9:00 04F3:3147 Touchpad" "libinput Tapping Enabled" 1
 xinput set-prop "DLL09D9:00 04F3:3147 Touchpad" "libinput Natural Scrolling Enabled" 1
 # blueman-applet &
