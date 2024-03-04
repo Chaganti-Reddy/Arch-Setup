3d99b34fef7e1b017f8e1098ba20a750c326d679
Chaganti Reddy <97330178+Chaganti-Reddy@users.noreply.github.com>
2024-03-04 14:41:37 +0530

    Adding Tap and natural scroll to my mouse script

--- home/user/.dwm/autostart.sh
+++ home/user/.dwm/autostart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 #
-xinput set-prop "DLL09D9:00 04F3:3147 Touchpad" "libinput Tapping Enabled" 1
-xinput set-prop "DLL09D9:00 04F3:3147 Touchpad" "libinput Natural Scrolling Enabled" 1
+# xinput set-prop "DLL09D9:00 04F3:3147 Touchpad" "libinput Tapping Enabled" 1
+# xinput set-prop "DLL09D9:00 04F3:3147 Touchpad" "libinput Natural Scrolling Enabled" 1
 /usr/lib/polkit-kde-authentication-agent-1 &
 /usr/bin/emacs --daemon &
 numlockx &
@@ -16,5 +16,6 @@ clipmenud &
 xcompmgr &
 mpd &
 slstatus &
+/home/ram/.dwm/scripts/mouse &
 /home/ram/.dwm/newlook &
 # /home/ram/.dwm/scripts/whatsapp_start &
