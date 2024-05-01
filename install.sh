6bae5e88608b9712886a2f02569b4588b3ab0e7f
Chaganti Reddy <97330178+Chaganti-Reddy@users.noreply.github.com>
2024-05-01 22:15:24 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@
 # rm -rf paru
 
 # 3. Install AUR packages
-# paru -S betterlockscreen-git brave-bin floorp-bin ccrypt didyoumean-git github-desktop-bin networkmanager-dmenu-git visual-studio-code-bin preload --noconfirm
+# paru -S betterlockscreen-git brave-bin floorp-bin ccrypt didyoumean-git github-desktop-bin networkmanager-dmenu-git visual-studio-code-bin preload python-pywalfox --noconfirm
 
 # 4. Install GUI packages
 # sudo pacman -S baobab gnome-disk-utility flameshot bc discord docker emacs gparted libreoffice-fresh lxappearance pavucontrol qutebrowser ranger yad telegram-desktop timeshift --noconfirm
@@ -35,38 +35,38 @@
 
 
 # 8. Mariadb setup
-sudo pacman -S mariadb --noconfirm
-sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-sudo systemctl enable mariadb
-sudo systemctl start mariadb
-sudo mariadb-secure-installation
+# sudo pacman -S mariadb --noconfirm
+# sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+# sudo systemctl enable mariadb
+# sudo systemctl start mariadb
+# sudo mariadb-secure-installation
 
 # 9. Enable services
-sudo updatedb
-sudo systemctl enable --now tlp
-sudo systemctl enable --now bluetooth.service
+# sudo updatedb
+# sudo systemctl enable --now tlp
+# sudo systemctl enable --now bluetooth.service
 
 # 10. Permissions
-sudo usermod -aG docker $USER
-sudo usermod -aG video $USER
+# sudo usermod -aG docker $USER
+# sudo usermod -aG video $USER
 
 # 11. Setup git 
-git config --global user.name "Chaganti-Reddy"
-git config --global user.email "chagantivenkataramireddy4@gmail.com"
+# git config --global user.name "Chaganti-Reddy"
+# git config --global user.email "chagantivenkataramireddy4@gmail.com"
 
 # 12. Tips:
 # 1. Install Java 
-sudo pacman -S jdk-openjdk openjdk-doc openjdk-src --noconfirm
+# sudo pacman -S jdk-openjdk openjdk-doc openjdk-src --noconfirm
 # 2. Install qbit torrent
 # sudo pacman -S qbittorrent --noconfirm
 # 3. Install qt6ct
-sudo pacman -S qt6ct --noconfirm
+# sudo pacman -S qt6ct --noconfirm
 # 4. Install Teamviewer
 # paru -S teamviewer --noconfirm
 # 5. Install Zathura
 # sudo pacman -S zathura zathura-pdf-mupdf zathura-djvu zathura-ps zathura-cb --noconfirm && paru -S zathura-pywal-git
 # 6. Install Okular 
-sudo pacman -S okular --noconfirm
+# sudo pacman -S okular --noconfirm
 # 7. Install Dolphin 
 # sudo pacman -S dolphin --noconfirm
 # 8. Install GTK theme and QT theme
@@ -74,36 +74,48 @@ sudo pacman -S okular --noconfirm
 # 9. Install anipy-cli 
 # paru -S anipy-cli-git --noconfirm
 # 10. Install Doom Emacs 
-git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs && ~/.config/emacs/bin/doom install
+# git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs && ~/.config/emacs/bin/doom install
 # 11. Insatll waldl from Extras folder of dotfiles
-cd ./Extras/Extras/waldl-master/ && sudo make install && cd ~/dotfiles
+# cd  ~/dotfiles/Extras/Extras/waldl-master/ && sudo make install && cd ~/dotfiles
 # 12. Install ollama from Extras folder of dotfiles
-sh ./Extras/Extras/ollama.sh
+# sh ~/dotfiles/Extras/Extras/ollama.sh
 # 13. Install brave Extensions
 # brave://extensions/ ---> Install Comp Companion, uBlock Origin, GFG to Github, Google Translate, LeetHub, User-Agent switcher
 # 14. Install Bash Language Server
-sudo npm i -g bash-language-server
+# sudo npm i -g bash-language-server
+# 15. Setup zsh shell as default
+# chsh -s /bin/zsh
+# 16. Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
 # 13. Move Respective files to root directory
-sudo cp ./Extras/Extras/usr/share/xsessions/dwm.desktop /usr/share/xsessions
-sudo cp -r ./Extras/Extras/boot/grub/themes/mocha /boot/grub/themes/
+# sudo cp ./Extras/Extras/usr/share/xsessions/dwm.desktop /usr/share/xsessions
+# sudo cp -r ./Extras/Extras/boot/grub/themes/mocha /boot/grub/themes/
 # Now edit the grub config file
-sudo cp ./Extras/Extras/etc/bash.bashrc /etc/
-sudo cp ./Extras/Extras/etc/DIR_COLORS /etc/
-sudo cp ./Extras/Extras/etc/mpd.conf /etc/
-sudo cp ./Extras/Extras/etc/nanorc /etc/
+# sudo cp ~/dotfiles/Extras/Extras/etc/bash.bashrc /etc/
+# sudo cp ~/dotfiles/Extras/Extras/etc/DIR_COLORS /etc/
+# sudo cp ~/dotfiles/Extras/Extras/etc/mpd.conf /etc/
+# sudo cp ~/dotfiles/Extras/Extras/etc/nanorc /etc/
 
 # 14. Setup PASS
 # gpg --full-generate-key
 # then setup pass init key
 
 # 15. Install MINICONDA
-wget https://repo.anaconda.com/miniconda/Miniconda3-py310_24.3.0-0-Linux-x86_64.sh
-bash Miniconda3-py310_24.3.0-0-Linux-x86_64.sh
-rm Miniconda3-py310_24.3.0-0-Linux-x86_64.sh
+# wget https://repo.anaconda.com/miniconda/Miniconda3-py310_24.3.0-0-Linux-x86_64.sh
+# bash Miniconda3-py310_24.3.0-0-Linux-x86_64.sh
+# rm Miniconda3-py310_24.3.0-0-Linux-x86_64.sh
 
 # 16. Install Fonts
-mkdir ~/.local/share/fonts/
-git clone https://github.com/Chaganti-Reddy/my-fonts.git
-cp -r my-fonts ~/.local/share/fonts/
+# mkdir ~/.local/share/fonts/
+# git clone https://github.com/Chaganti-Reddy/my-fonts.git
+# cp -r my-fonts ~/.local/share/fonts/
+# rm -rf my-fonts
+
+# 17. Install/stow dotfiles
+# First check for conflicts
+# rm -rf ~/.config/doom
+# rm ~/.bashrc
+# cd ~/dotfiles
+# stow */
