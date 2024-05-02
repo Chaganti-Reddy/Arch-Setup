4aaf53ebb8633af6ec41c874cc58dda193269d05
Chaganti Reddy <97330178+Chaganti-Reddy@users.noreply.github.com>
2024-05-02 12:05:20 +0530

    Update autostart.sh

--- DWMScripts/.dwm/autostart.sh
+++ DWMScripts/.dwm/autostart.sh
@@ -3,7 +3,7 @@
 #
 # xinput set-prop "DLL09D9:00 04F3:3147 Touchpad" "libinput Tapping Enabled" 1
 # xinput set-prop "DLL09D9:00 04F3:3147 Touchpad" "libinput Natural Scrolling Enabled" 1
-/usr/lib/polkit-kde-authentication-agent-1 &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/bin/emacs --daemon &
 numlockx &
 nm-applet &
@@ -23,3 +23,8 @@ if [ -d ~/.cache/wallheaven ]; then
 fi
 ~/.dwm/newlook &
 # /home/ram/.dwm/scripts/whatsapp_start &
+/usr/bin/gnome-keyring-daemon --start
+export SSH_AUTH_SOCK
+export GPG_AGENT_INFO
+export GNOME_KEYRING_CONTROL
+export GNOME_KEYRING_PID
