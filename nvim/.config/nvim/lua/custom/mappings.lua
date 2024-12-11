e67e2b344b71d7c1fa8d4bb305666d4690e98234
Chaganti Reddy <97330178+Chaganti-Reddy@users.noreply.github.com>
2024-12-11 16:40:12 +0530

    Update mappings.lua

--- nvim/.config/nvim/lua/custom/mappings.lua
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -19,6 +19,16 @@ M.general = {
     -- Enter Zen Mode using z 
     ["<leader>z"] = { ":ZenMode<CR>", "zen mode" },
     ["<leader>gg"] = { ":Gen<CR>", "Open Ollama" },
+
+    -- CompetiTest
+    ["<leader>pr"] = { "<cmd>CompetiTest run<cr>", "Competitest run" },
+    ["<leader>pa"] = { "<cmd>CompetiTest receive testcases<cr>", "Competitest Receive Testcases" },
+    ["<leader>pc"] = { "<cmd>CompetiTest receive contest<cr>", "Competitest Receive Contest" },
+    ["<leader>pp"] = { "<cmd>CompetiTest receive problem<cr>", "Competitest Receive Problem" },
+    ["<leader>pu"] = { "<cmd>CompetiTest show_ui<cr>", "Competitest Show UI" },
+    ["<leader>pd"] = { "<cmd>CompetiTest delete_testcase<cr>", "Competitest Delete Testcase" },
+    ["<leader>pA"] = { "<cmd>CompetiTest add_testcase<cr>", "Competitest Add Testcase" },
+    ["<leader>pe"] = { "<cmd>CompetiTest edit_testcase<cr>", "Competitest Edit Testcase" },
   },
   v = {
     [">"] = { ">gv", "indent" },
