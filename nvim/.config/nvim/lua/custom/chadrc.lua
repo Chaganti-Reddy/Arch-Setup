dd7792af202b988f44c82fcbb91174201a41f7d0
Chaganti Reddy <97330178+Chaganti-Reddy@users.noreply.github.com>
2025-01-07 14:34:24 +0530

    Update chadrc.lua

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -5,7 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "nightowl",
+  theme = "cozy",
   theme_toggle = { "nightowl", "one_light" },
 
   hl_override = highlights.override,
