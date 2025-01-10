a1ed9fdb2acdd31463331e62558f78485c45895a
Chaganti Reddy <97330178+Chaganti-Reddy@users.noreply.github.com>
2025-01-10 16:07:17 +0530

    update waybar/.config/waybar/switch_style2.sh

--- waybar/.config/waybar/switch_style2.sh
+++ waybar/.config/waybar/switch_style2.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 killall waybar
 SDIR="$HOME/.config/waybar"
-waybar -c "$SDIR"/config2 -s "$SDIR"/style2.css &
-swaymsg gaps outer all set 0 && swaymsg gaps inner all set 5
-
+waybar -c "$SDIR"/config2.jsonc -s "$SDIR"/style2.css &
